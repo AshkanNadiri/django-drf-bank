1aaf31777ecfb6191cef0753356c291c6869c08d
geekwise-ashkan-nadiri <geekwise.ashkan.nadiri@gmail.com>
2019-12-11 21:01:26 +0000

    finished: finished with serializers

--- app/bank/views.py
+++ app/bank/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User,  Group
+from rest_framework import viewsets
+from .serializer import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows you to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    class_serializer = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows you to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    class_serializer = GroupSerializer
+
+
