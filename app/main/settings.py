c3e60516c367b0aa83368e110c9ca69e05e30af9
geekwise-ashkan-nadiri <geekwise.ashkan.nadiri@gmail.com>
2019-12-11 23:04:58 +0000

    Added:  added pagination and apps

--- app/main/settings.py
+++ app/main/settings.py
@@ -29,6 +29,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE' : 10
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'bank'
 ]
 
 MIDDLEWARE = [
